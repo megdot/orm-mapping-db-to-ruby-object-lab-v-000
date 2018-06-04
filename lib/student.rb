1ab1fb0a2bbbdf6e8edd4eec36fa40289ae47a40
Megan Dotto <megan.dotto@gmail.com>
2018-06-04 04:22:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,19 +80,31 @@ class Student
     end
   end
 
-def self.students_below_12th_grade
+  def self.students_below_12th_grade
 
-  sql = <<-SQL
-  SELECT *
-  FROM students
-  WHERE grade < 12;
-  SQL
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade < 12;
+    SQL
 
-  DB[:conn].execute(sql).collect do |row|
-    self.new_from_db(row)
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
   end
 
-end
+  def self.all_students_in_grade_X(grade_x)
+
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?;
+    SQL
+
+    DB[:conn].execute(sql, grade_x).collect do |row|
+      self.new_from_db(row)
+    end
+  end
 
   def save
     sql = <<-SQL
